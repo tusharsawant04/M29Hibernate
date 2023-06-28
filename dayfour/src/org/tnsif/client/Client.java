d0ebec02fc80c8ca6d7d205f079357ac3e64096d
tusharsawant04 <tsawant379@gmail.com>
2023-06-28 16:09:31 +0530

    first commit

---
+++ dayfour/src/org/tnsif/client/Client.java
@@ -0,0 +1,30 @@
+//driver class
+//program to demonstrate on JPA Queries
+package org.tnsif.client;
+import org.tnsif.service.BookService;
+import org.tnsif.service.BookServiceImpl;
+public class Client {
+
+	public static void main(String[] args) {
+		BookService service=new BookServiceImpl();
+		
+		System.out.println("Book with specific ID:");
+		System.out.println(service.getBookByID(101));
+		
+		System.out.println("Book with specific Author:");
+		System.out.println(service.getAuthorBooks("Dennis Ritchie"));
+		
+		System.out.println("Book with specific title:");
+		System.out.println(service.getBookByTitle("AI"));
+		
+		System.out.println("Book Count:");
+		System.out.println(service.getBookCount());
+		
+		System.out.println("Book with specific price range:");
+		System.out.println(service.getBookByPrice(300, 500));
+
+		
+
+	}
+
+}
